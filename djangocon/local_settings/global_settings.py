3a843127321dfa5fe4f8e66656ea8ccb68c1c403
Idan Gazit <idan@pixane.com>
2010-01-03 02:42:44 +0200

    MEDIA_* point to media not static

--- djangocon/local_settings/global_settings.py
+++ djangocon/local_settings/global_settings.py
@@ -37,12 +37,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'static')
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
