f1e653241980a97bc0265e7220a7050aef3a42f1
Idan Gazit <idan@pixane.com>
2010-01-03 02:11:46 +0200

    Fixing global MEDIA_ROOT/MEDIA_URL definitions

--- djangocon/local_settings/global_settings.py
+++ djangocon/local_settings/global_settings.py
@@ -37,12 +37,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = os.path.join(PROJECT_DIR, 'media')
+MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
