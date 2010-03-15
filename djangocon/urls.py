cc6a7ec2b9d3d389be101bb4830176d4ebeafdc2
Idan Gazit <idan@pixane.com>
2010-03-15 13:41:51 +0200

    Adding new homepage view.

--- djangocon/urls.py
+++ djangocon/urls.py
@@ -12,14 +12,14 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('djangocon',
-#    url(r'^$', 'subscribers.views.home', name='home'),
+    url(r'^$', 'utils.views.home', name='home'),
     url(r'^clear/$', 'subscribers.views.clear', name='clear'),
     url(r'^talks/', include('djangocon.talks.urls'), name='talks'),
     url(r'^attendees/', include('djangocon.attendees.urls'), name='attendees'),
 )
 
 urlpatterns += patterns('django.views.generic.simple',
-    url(r'^$', 'direct_to_template', {'template': 'home.html'}, name='home'),
+#    url(r'^$', 'direct_to_template', {'template': 'home.html'}, name='home'),
     url(r'^legal/$', 'direct_to_template', {'template': 'legal_notices.html'}, name='legal_notices'),
     url(r'^register/$', 'direct_to_template', {'template': 'register.html'}, name='register'),
 )
--- djangocon/utils/views.py
+++ djangocon/utils/views.py
@@ -1,6 +1,9 @@
 from django import http
 from django.conf import settings
-from django.template import Context, loader
+from django.template import RequestContext, Context, loader
+from django.shortcuts import render_to_response
+
+from djangocon.subscribers.models import Tagline
 
 def server_error(request, template_name='500.html'):
     """
@@ -18,3 +21,21 @@ def server_error(request, template_name='500.html'):
         'MEDIA_URL': settings.MEDIA_URL,
         'STATIC_URL': settings.STATIC_URL,
     })))
+
+def home(request):
+    """
+    Renders the homepage.
+    
+    Templates: `home.html`
+    Context:
+        
+    """
+    
+    context = {}
+    context['taglines'] = Tagline.objects.order_by('?')[:50]
+    
+    return render_to_response(
+        'home.html',
+        context,
+        context_instance=RequestContext(request))
+    
